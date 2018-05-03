6fbc1df7dcda596fff2adaa82e5c702fdcfb015a
Hayley Landsberg <hayleyheath@gmail.com>
2018-05-03 11:47:48 -0500

    Archive button added. It clears the card from the dom and pushing object into the array

--- src/scripts/dropFunction.js
+++ src/scripts/dropFunction.js
@@ -1,15 +1,36 @@
+const archiveArray = require("./loadArchiveData")
+const taskArray = require("./localStorageLoad")
+const idMaker = require("./idGenerator")
+
 function todoBackground(data, section) {
     const card = document.querySelector(`#${data}`)
     const lane = section.id
     switch (lane) {
         case "doing":
-        card.classList.add("redBackground")
-        card.classList.remove("yellowBackground")
-        break;
+            card.classList.add("redBackground")
+            card.classList.remove("yellowBackground")
+            $(".archive").remove();
+            break;
         case "done":
-        card.classList.add("yellowBackground")
-        card.classList.remove("redBackground")
-        break;
+            card.classList.add("yellowBackground")
+            card.classList.remove("redBackground")
+            const archiveButton = () => {
+                const button = document.createElement("button")
+                button.classList = "archive"
+                button.textContent = "Archive"
+                card.appendChild(button)
+            }
+            archiveButton()
+            //Deletes the card when you click the archive button. Then it will move the card information into the archive array
+            document.querySelector(".archive").addEventListener("click", function(){
+                $(".card-section").remove()
+                const taskName = event.target.parentNode.querySelector(".task-name").textContent
+                console.log(taskName)
+                const findTask = taskArray.find(t => t.taskName === taskName)
+                archiveArray.push(findTask)
+                localStorage.setItem("archiveArray", JSON.stringify(archiveArray))
+            });
+            break;
     }
 }
 
@@ -23,7 +44,7 @@ const DragDropManager = Object.create(null, {
             targets.forEach(target => {
                 // Dragover not supported by default. Turn that off.
                 target.ondragover = e => e.preventDefault()
-                
+
                 target.ondrop = (e) => {
                     // Enabled dropping on targets
                     e.preventDefault()
@@ -32,7 +53,7 @@ const DragDropManager = Object.create(null, {
                     const data = e.dataTransfer.getData("text")
                     // Append card to target component as child
                     // TODO: This should only happen if the target has no children nodes
-                     // TODO: This should not happen if the target is another stage card
+                    // TODO: This should not happen if the target is another stage card
                     target.appendChild(document.getElementById(data))
                     todoBackground(data, target)
                 }
